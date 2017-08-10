726276b473cbd06be16819a370e55343eb3274f7
animeshkuzur <animesh.kuzur@outlook.com>
2017-08-10 10:27:34 +0530

    update 10/8

---
+++ 10-08-2017/gauss-elemination.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+float fx(float x){
+	return ((x*x*x)-(4*x)-9);
+}
+
+/*void secant(float a,float b){
+	float m=0;int q=0;
+	
+}*/
+
+int main(){
+	return 0;
+}
--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 ### Lab 2 `03-08-2017`
 - [ ] Newton-Raphson Method
 - [ ] Muller Method
+
+### Lab 3 `10-08-2017`
+- [ ] Gauss Elemination Method 
